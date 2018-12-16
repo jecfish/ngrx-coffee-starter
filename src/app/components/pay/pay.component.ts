f42cbff585fcebba6aab636549061dccd7d6102b
Jecelyn <jecelyn.yeen@randstad.com.my>
2018-12-16 14:59:24 +0800

    explaination

--- src/app/components/pay/pay.component.ts
+++ src/app/components/pay/pay.component.ts
@@ -13,8 +13,31 @@ import { EmptyCart } from '../../state/app.actions';
 })
 export class PayComponent implements OnInit {
 
-  private coffeeList$ = this.store.pipe(select(x => x.app.coffeeList));
-  private cart$ = this.store.pipe(select(x => x.app.cart));
+  private coffeeList$ = this.store
+    .pipe(select(x => x.app.coffeeList));
+
+  private cart$ = this.store
+    .pipe(select(x => x.app.cart));
+
+  // example
+  // cart: [{itemA 2}, {item B 4, itemC 6}]
+  // coffeeList: [itemA $10, itemB $20, itemC $100]
+
+  // end result total: $20 + $80 + $600 = $700
+
+  // map(([list, cart])
+  // [
+  //  list：[itemA $10, itemB $20, itemC $100],
+  //  cart: [{itemA 2}, {item B 4, itemC 6}]
+  // ]
+
+  // cart.map
+  // [ $20, $80, $600 ]
+
+  // x.reduce
+  // loop1: 0 + $20 = $20
+  // loop2: $20 + $80 = $100
+  // loop3: $100 + $600 = $700
 
   total$ = combineLatest(this.coffeeList$, this.cart$).pipe(
     map(([list, cart]) => cart.map(c => list.find(x => x.name === c.name).price * c.quantity)),
